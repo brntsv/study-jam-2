db8e2c1d9ae8acb6601b83f0ab71f6a93c5c054e
Nikita Barantsev <nbarentsev@gmail.com>
2023-01-25 00:26:25 +0700

    upd bloc and style auth

--- lib/features/auth/screens/view/auth_screen.dart
+++ lib/features/auth/screens/view/auth_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:surf_practice_chat_flutter/features/auth/models/token_dto.dart';
 import 'package:surf_practice_chat_flutter/features/auth/repository/auth_repository.dart';
 import 'package:surf_practice_chat_flutter/features/auth/screens/bloc/auth_screen_bloc.dart';
+import 'package:surf_practice_chat_flutter/features/chat/repository/chat_repository.dart';
+import 'package:surf_practice_chat_flutter/features/chat/screens/chat_screen.dart';
+import 'package:surf_practice_chat_flutter/token.dart';
 import 'package:surf_study_jam/surf_study_jam.dart';
 
 /// Screen for authorization process.
@@ -48,8 +53,16 @@ class AuthScreen extends StatelessWidget {
     ScaffoldMessenger.of(context)
       ..hideCurrentSnackBar()
       ..showSnackBar(
-        const SnackBar(
-          content: Text('Error, invalid login or password'),
+        SnackBar(
+          content: Row(
+            children: const [
+              Icon(
+                Icons.error,
+                color: Colors.red,
+              ),
+              Text(' Error: invalid login or password'),
+            ],
+          ),
         ),
       );
   }
@@ -60,6 +73,21 @@ class AuthScreen extends StatelessWidget {
       // TopicsPage(client: client);
     }));
   }
+
+  // void _pushToChat(BuildContext context, TokenDto token) {
+  //   Navigator.push<ChatScreen>(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (_) {
+  //         return ChatScreen(
+  //           chatRepository: ChatRepository(
+  //             StudyJamClient().getAuthorizedClient(token.token),
+  //           ),
+  //         );
+  //       },
+  //     ),
+  //   );
+  // }
 }
 
 class AuthView extends StatelessWidget {
@@ -68,19 +96,23 @@ class AuthView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = context.read<AuthScreenBloc>();
+    String initLogin = 'brntsv';
+    String initPassword = 'u1ME9HAhVhWj';
 
     return Scaffold(
       body: SafeArea(
           child: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(12.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextFormField(
-              initialValue: 'brntsv',
-              decoration: const InputDecoration(
-                labelText: 'Login',
-                border: OutlineInputBorder(),
+              initialValue: initLogin,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.person),
+                labelText: 'Логин',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
               ),
               onChanged: (value) => {
                 bloc.add(
@@ -89,13 +121,15 @@ class AuthView extends StatelessWidget {
               },
             ),
             const SizedBox(
-              height: 8,
+              height: 10,
             ),
             TextFormField(
-              initialValue: 'u1ME9HAhVhWj',
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
+              initialValue: initPassword,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.lock),
+                labelText: 'Пароль',
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
               ),
               onChanged: (value) => {
                 bloc.add(
@@ -106,9 +140,12 @@ class AuthView extends StatelessWidget {
             const SizedBox(
               height: 8,
             ),
-            ElevatedButton(
-              onPressed: () => {bloc.add(AuthScreenContinueClicked())},
-              child: const Text('Continue'),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () => {bloc.add(AuthScreenContinueClicked())},
+                child: const Text('ДАЛЕЕ'),
+              ),
             ),
           ],
         ),
